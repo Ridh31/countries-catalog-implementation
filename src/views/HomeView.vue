36cb99572b80ae3ec57c25991aaa6f9de464c802
BACK-END.05 <BACK-END.05@TURBOTECH.COM>
2024-02-19 16:50:17 +0700

    Bugs: fix styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="flex justify-center items-center">
+      <img alt="Vue logo" src="../assets/images/logo.png">
+    </div>
+    <AppHeader msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  // @ is an alias to /src
+  import AppHeader from '@/components/AppHeader.vue'
+
+  export default {
+    name: 'HomeView',
+    components: {
+      AppHeader
+    }
   }
-}
+
 </script>
