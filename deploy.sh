8a5e35c9b9b43dd714c49b56ed495d9f29cbc035
Ridh31 <himroungridh2001@gmail.com>
2024-02-21 20:32:26 +0700

    Update deploy files

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push https://github.com/Ridh31/countries-catalog-implementation.git
+git push --set-upstream https://github.com/Ridh31/countries-catalog-implementation.git gh-pages
 
 cd -
