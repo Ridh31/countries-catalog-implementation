39bab31909dd05c4f14f0b301567ec9ec5c60d54
BACK-END.05 <BACK-END.05@TURBOTECH.COM>
2024-02-21 10:09:57 +0700

    BG: remove flip & implement gradient

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col gap-5">
 
       <!-- Search & Sort -->
-      <div class="grid grid-cols-2">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
 
         <!-- Search -->
         <div class="flex justify-start items-center">
@@ -13,7 +13,7 @@
 
         <!-- Sort -->
         <div class="flex justify-end items-center">
-          <select name="" id="" class="focus:outline-none max-w-sm w-full p-3 shadow-md rounded-md">
+          <select name="" id="" class="focus:outline-none max-w-sm w-52 md:w-full p-3 shadow-md rounded-md">
             <option value="" selected disabled>Sort (Country name)</option>
             <option value="asc">Asending</option>
             <option value="desc">Decending</option>
@@ -24,56 +24,54 @@
       <!-- Catalog -->
       <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-5">
         <li v-for="country in countries" :key="country"
-          class="flip-card">
-          <div class="flip-card-inner flex flex-col gap-3 bg-white p-5 shadow-md rounded-md cursor-pointer">
+          class="flex flex-col gap-3 bg-gradient-to-r from-white hover:from-blue-100 to-white hover:to-cyan-100 p-5 shadow-md rounded-md cursor-pointer hover:scale-105 transition delay-75">
 
-            <!-- Image -->
-            <div class="select-none border border-gray-200 flex-shrink-0 rounded-sm hover:scale-95 transition delay-75">
-              <img class="object-cover h-48 w-full" :src="country.flags.png ?? ''" :alt="country.name.common ?? ''">
-            </div>
+          <!-- Image -->
+          <div class="select-none border border-gray-200 flex-shrink-0 rounded-sm hover:scale-95 transition delay-75">
+            <img class="object-cover h-48 w-full" :src="country.flags.png ?? ''" :alt="country.name.common ?? ''">
+          </div>
 
-            <!-- Official name -->
-            <div class="flex gap-1 text-start">
-              <span class="select-none font-semibold whitespace-nowrap">Official Name: </span>
-              <span class="text-red-400 line-clamp-1">{{ country.name.official ?? "" }}</span>
-            </div>
+          <!-- Official name -->
+          <div class="flex gap-1 text-start">
+            <span class="select-none font-semibold whitespace-nowrap">Official Name: </span>
+            <span class="text-red-400 line-clamp-1">{{ country.name.official ?? "" }}</span>
+          </div>
 
-            <!-- cca2 -->
-            <div class="flex gap-1 text-start">
-              <span class="select-none font-semibold whitespace-nowrap">CCA2: </span>
-              <span class="bg-blue-200 px-3 py-0.5 rounded-xl">{{ country.cca2 ?? "" }}</span>
-            </div>
+          <!-- cca2 -->
+          <div class="flex gap-1 text-start">
+            <span class="select-none font-semibold whitespace-nowrap">CCA2: </span>
+            <span class="bg-blue-200 px-3 py-0.5 rounded-xl">{{ country.cca2 ?? "" }}</span>
+          </div>
 
-            <!-- cca3 -->
-            <div class="flex gap-1 text-start">
-              <span class="select-none font-semibold whitespace-nowrap">CCA3: </span>
-              <span class="bg-green-200 px-3 py-0.5 rounded-xl">{{ country.cca3 ?? "" }}</span>
-            </div>
+          <!-- cca3 -->
+          <div class="flex gap-1 text-start">
+            <span class="select-none font-semibold whitespace-nowrap">CCA3: </span>
+            <span class="bg-green-200 px-3 py-0.5 rounded-xl">{{ country.cca3 ?? "" }}</span>
+          </div>
 
-            <!-- Native name -->
-            <div class="flex gap-1 text-start">
-              <span class="select-none font-semibold whitespace-nowrap">Native name: </span>
-              <span v-for="native in country.name.nativeName" :key="native" class="line-clamp-1">
-                {{ native.official ?? "" }}
-              </span>
-            </div>
+          <!-- Native name -->
+          <div class="flex gap-1 text-start">
+            <span class="select-none font-semibold whitespace-nowrap">Native name: </span>
+            <span v-for="native in country.name.nativeName" :key="native" class="line-clamp-1">
+              {{ native.official ?? "" }}
+            </span>
+          </div>
 
-            <!-- ALT spellings -->
-            <div class="flex gap-1 text-start">
-              <span class="select-none font-semibold whitespace-nowrap">ALT Spellings: </span>
-              <span class="line-clamp-1">
-                {{ country.altSpellings.join(", ") ?? "" }}
-              </span>
-            </div>
+          <!-- ALT spellings -->
+          <div class="flex gap-1 text-start">
+            <span class="select-none font-semibold whitespace-nowrap">ALT Spellings: </span>
+            <span class="line-clamp-1">
+              {{ country.altSpellings.join(", ") ?? "" }}
+            </span>
+          </div>
 
-            <!-- idd -->
-            <div class="flex gap-1 text-start">
-              <span class="select-none font-semibold">International Direct Dialing: </span>
-              <span class="bg-gray-200 px-3 py-0.5 rounded-xl">{{ country.idd.root ?? "" }}</span>
-              <span class="bg-gray-200 ml-1 px-3 py-0.5 rounded-xl line-clamp-1">
-                {{ String(country.idd.suffixes).split(",").join(", ") ?? "" }}
-              </span>
-            </div>
+          <!-- International Direct Dialing -->
+          <div class="flex gap-1 text-start">
+            <span class="select-none font-semibold">IDD: </span>
+            <span class="bg-gray-200 px-3 py-0.5 rounded-xl">{{ country.idd.root ?? "" }}</span>
+            <span class="flex bg-gray-200 ml-1 px-3 py-0.5 rounded-xl line-clamp-1">
+              {{ String(country.idd.suffixes).split(",").join(", ") ?? "" }}
+            </span>
           </div>
         </li>
       </ul>
@@ -129,6 +127,9 @@ export default {
 <style>
 
 .flip-card {
+  background-color: transparent;
+  width: 300px;
+  height: 300px;
   perspective: 1000px;
 }
 
