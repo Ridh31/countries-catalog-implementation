17592af27df72ddb421db879a8a7c07c096f0947
BACK-END.05 <BACK-END.05@TURBOTECH.COM>
2024-02-21 09:47:09 +0700

    Card: implement flip transition

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -24,54 +24,56 @@
       <!-- Catalog -->
       <ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-5">
         <li v-for="country in countries" :key="country"
-          class="flex flex-col gap-3 bg-white p-5 shadow-md rounded-md cursor-pointer hover:scale-105 transition delay-75">
+          class="flip-card">
+          <div class="flip-card-inner flex flex-col gap-3 bg-white p-5 shadow-md rounded-md cursor-pointer">
 
-          <!-- Image -->
-          <div class="select-none flex-shrink-0">
-            <img class="w-full h-20 object-contain" :src="country.flags.png ?? ''" :alt="country.name.common ?? ''">
-          </div>
+            <!-- Image -->
+            <div class="select-none border border-gray-200 flex-shrink-0 rounded-sm hover:scale-95 transition delay-75">
+              <img class="object-cover h-48 w-full" :src="country.flags.png ?? ''" :alt="country.name.common ?? ''">
+            </div>
 
-          <!-- Official name -->
-          <div class="flex gap-1 text-start">
-            <span class="select-none font-semibold whitespace-nowrap">Official Name: </span>
-            <span class="text-red-400 line-clamp-1">{{ country.name.official ?? "" }}</span>
-          </div>
+            <!-- Official name -->
+            <div class="flex gap-1 text-start">
+              <span class="select-none font-semibold whitespace-nowrap">Official Name: </span>
+              <span class="text-red-400 line-clamp-1">{{ country.name.official ?? "" }}</span>
+            </div>
 
-          <!-- cca2 -->
-          <div class="flex gap-1 text-start">
-            <span class="select-none font-semibold whitespace-nowrap">CCA2: </span>
-            <span class="bg-blue-200 px-3 py-0.5 rounded-xl">{{ country.cca2 ?? "" }}</span>
-          </div>
+            <!-- cca2 -->
+            <div class="flex gap-1 text-start">
+              <span class="select-none font-semibold whitespace-nowrap">CCA2: </span>
+              <span class="bg-blue-200 px-3 py-0.5 rounded-xl">{{ country.cca2 ?? "" }}</span>
+            </div>
 
-          <!-- cca3 -->
-          <div class="flex gap-1 text-start">
-            <span class="select-none font-semibold whitespace-nowrap">CCA3: </span>
-            <span class="bg-green-200 px-3 py-0.5 rounded-xl">{{ country.cca3 ?? "" }}</span>
-          </div>
+            <!-- cca3 -->
+            <div class="flex gap-1 text-start">
+              <span class="select-none font-semibold whitespace-nowrap">CCA3: </span>
+              <span class="bg-green-200 px-3 py-0.5 rounded-xl">{{ country.cca3 ?? "" }}</span>
+            </div>
 
-          <!-- Native name -->
-          <div class="flex gap-1 text-start">
-            <span class="select-none font-semibold whitespace-nowrap">Native name: </span>
-            <span v-for="native in country.name.nativeName" :key="native" class="line-clamp-1">
-              {{ native.official ?? "" }}
-            </span>
-          </div>
+            <!-- Native name -->
+            <div class="flex gap-1 text-start">
+              <span class="select-none font-semibold whitespace-nowrap">Native name: </span>
+              <span v-for="native in country.name.nativeName" :key="native" class="line-clamp-1">
+                {{ native.official ?? "" }}
+              </span>
+            </div>
 
-          <!-- ALT spellings -->
-          <div class="flex gap-1 text-start">
-            <span class="select-none font-semibold whitespace-nowrap">ALT Spellings: </span>
-            <span class="line-clamp-1">
-              {{ country.altSpellings.join(", ") ?? "" }}
-            </span>
-          </div>
+            <!-- ALT spellings -->
+            <div class="flex gap-1 text-start">
+              <span class="select-none font-semibold whitespace-nowrap">ALT Spellings: </span>
+              <span class="line-clamp-1">
+                {{ country.altSpellings.join(", ") ?? "" }}
+              </span>
+            </div>
 
-          <!-- idd -->
-          <div class="flex gap-1 text-start">
-            <span class="select-none font-semibold">International Direct Dialing: </span>
-            <span class="bg-gray-200 px-3 py-0.5 rounded-xl">{{ country.idd.root ?? "" }}</span>
-            <span class="bg-gray-200 ml-1 px-3 py-0.5 rounded-xl line-clamp-1">
-              {{ String(country.idd.suffixes).split(",").join(", ") ?? "" }}
-            </span>
+            <!-- idd -->
+            <div class="flex gap-1 text-start">
+              <span class="select-none font-semibold">International Direct Dialing: </span>
+              <span class="bg-gray-200 px-3 py-0.5 rounded-xl">{{ country.idd.root ?? "" }}</span>
+              <span class="bg-gray-200 ml-1 px-3 py-0.5 rounded-xl line-clamp-1">
+                {{ String(country.idd.suffixes).split(",").join(", ") ?? "" }}
+              </span>
+            </div>
           </div>
         </li>
       </ul>
@@ -123,3 +125,44 @@ export default {
 }
 
 </script>
+
+<style>
+
+.flip-card {
+  perspective: 1000px;
+}
+
+.flip-card-inner {
+  position: relative;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+  backface-visibility: hidden;
+  -moz-backface-visibility: hidden;
+}
+
+.flip-card:focus {
+    outline: 0;
+}
+
+.flip-card:hover .flip-card-inner,
+.flip-card:focus .flip-card-inner{
+  transform: rotateY(180deg);
+}
+
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+.flip-card-front {
+  z-index: 2;
+}
+
+.flip-card-back {
+  transform: rotateY(180deg);
+  z-index: 1;
+}
+
+</style>
