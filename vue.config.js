dcee5e1785d36f647191cb1d8eec2dd00e49a9ce
Ridh31 <himroungridh2001@gmail.com>
2024-02-21 22:06:28 +0700

    dist update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/counties-catalog-implementation/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/countries-catalog-implementation/' : '/'
 }
